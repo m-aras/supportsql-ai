f3241ce9ae336adf2a61b212a764a1ee02540d72
m-aras <152270896+m-aras@users.noreply.github.com>
2026-09-09 01:02:34 +0300

    Update db.py to load DATABASE_URL from .env
    Refactor database connection to use environment variables.

--- db.py
+++ db.py
@@ -1,14 +1,7 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 
-DB_HOST = "localhost"
-DB_PORT = "5432"
-DB_NAME = "dvdrental"
-DB_USER = "postgres"
-DB_PASSWORD = "1234"
-
-DATABASE_URL = (
-    f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}"
-    f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-)
-
+load_dotenv()
+DATABASE_URL = os.environ["DATABASE_URL"]
 engine = create_engine(DATABASE_URL)
